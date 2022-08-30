e298acb52126223ec48204ff7cead6a3b85a5196
Kbgeptupsi <cristianalarcon109@gmail.com>
2022-08-30 13:57:25 -0500

    nav bar in views

--- ConsultarClientes.php
+++ ConsultarClientes.php
@@ -30,32 +30,7 @@ $Usuario = $_SESSION['Usuario'];
 </head>
 
 <body class="sb-nav-fixed">
-	<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-		<a class="navbar-brand" href="index.php">Control Gym</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-		
-		<ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $Usuario; ?><i class="fas fa-user fa-fw"></i></a>
-				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-					<a class="dropdown-item" href="#">Configuración</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="logout.php">Salir</a>
-				</div>
-			</li>
-		</ul>
-	</nav>
-	<div id="layoutSidenav">
-		<div id="layoutSidenav_nav">
-			<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-				<div class="sb-sidenav-menu">
-					<div class="nav">
-						<div class="sb-sidenav-menu-heading">Consultas</div>
-						<a class="nav-link" href="principal.php">
-							<div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-							Menu Principal
-						</a>
-					</div>
-				</div>
+	<?php require_once('V_Nav.php')?>
 				<div class="sb-sidenav-footer">
 					<div class="small">Estas en:</div>
 					Consultas
--- GestionCliente.php
+++ GestionCliente.php
@@ -29,63 +29,7 @@ $Usuario = $_SESSION['Usuario'];
 
 <body class="sb-nav-fixed">
 	
-	<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-		<a class="navbar-brand" href="index.php">Control Gym</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-
-		<ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $Usuario; ?><i class="fas fa-user fa-fw"></i></a>
-				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-					<a class="dropdown-item" href="#">Configuración</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="logout.php">Salir</a>
-				</div>
-			</li>
-		</ul>
-	</nav>
-
-
-	<div id="layoutSidenav">
-		<div id="layoutSidenav_nav">
-			<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-				<div class="sb-sidenav-menu">
-					<div class="nav">
-						<div class="sb-sidenav-menu-heading">Gestión de Clientes</div>
-						<a class="nav-link" href="principal.php">
-							<div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-							Menu Principal
-						</a>
-						<div class="sb-sidenav-menu-heading">Interfaz Principal</div>
-
-						<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-							<div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-							Gestión Gimnasio
-							<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-						</a>
-						<div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
-							<nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-								<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">Area cliente
-									<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div></a>
-									<div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-										<nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="GestionCliente.php">Registro de clientes</a><a class="nav-link" href="ConsultarClientes.php">Consulta de clientes</a></nav>
-									</div>
-									<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">Area contable
-										<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div></a>
-										<div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-											<nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="ConsultarFacturas.php">Consulta de facturas</a></nav>
-										</div>
-									</nav>
-								</div>
-								
-							</a>
-						</div>
-					</div>
-					<div class="sb-sidenav-footer">
-						<div class="small">Estas en:</div>
-						Gestión de Clientes
-					</div>
-				</nav>
-			</div>
+            <?php require_once('V_NavBar.php');?>
 			<div id="layoutSidenav_content">
 				<main>
 					<div class="container-fluid">
@@ -239,12 +183,7 @@ $Usuario = $_SESSION['Usuario'];
 					<?php require_once('V_Footer_Dev.php')?>
 				</div>
 			</div>
-			<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-			<script src="js/scripts.js"></script>
-			<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-			<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-			<script src="assets/demo/datatables-demo.js"></script>
+			<?php require_once('C_Scripts.php')?>
 		</body>
 
 		</html>
